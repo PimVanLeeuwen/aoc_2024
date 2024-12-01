cf181b2fca6b3d80d65845bec2b7c3364813b4d1
Pim van Leeuwen <git@pvleeuwen.com>
2024-12-01 06:14:20 +0100

    Completed day 1

--- day_1/Day1.cpp
+++ day_1/Day1.cpp
@@ -1,5 +1,6 @@
 #include "Day1.h"
 
+#include <algorithm>
 #include <iostream>
 #include <ostream>
 #include <sstream>
@@ -7,18 +8,31 @@
 using namespace std;
 
 void Day1::execute(const vector<string>& lines) {
+    vector<int> left;
+    vector<int> right;
+
+    // get input
     for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line);
-        string token;
-
-        // put the points in here
-        vector<string> points;
-
-        // separate line
-        while (getline(ss, token, ',')) { points.push_back(token); }
+        left.push_back(stoi(line.substr(0, 5)));
+        right.push_back(stoi(line.substr(7, 6)));
     }
 
-    cout << "TODO IMPLEMENT" << endl;
+    // sort both vectors
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+
+    // do part 1
+    long part_1 = 0;
+    for (int i = 0; i < left.size(); i++) { part_1 += abs(left[i]-right[i]); }
+    cout << "Part 1: " << part_1 << endl;
+
+    // do part 2
+    long part_2 = 0;
+    for (int i = 0; i < left.size(); i++) {
+        int times = 0;
+        for (int j = 0; j < right.size(); j++) { if (right[j] == left[i]) times++; }
+        part_2 += left[i]*times;
+    }
+    cout << "Part 2: " << part_2 << endl;
 }
 
