a8d788cba971a977911389b2f93c91e6ffe04201
Pim van Leeuwen <git@pvleeuwen.com>
2024-12-06 06:44:42 +0100

    Completed day 6, not the most efficient solution but c++ runs fast enough

--- day_6/Day6.cpp
+++ day_6/Day6.cpp
@@ -1,22 +1,119 @@
 #include "Day6.h"
 
 #include <iostream>
+#include <map>
+#include <set>
 #include <sstream>
 
 using namespace std;
 
 void Day6::execute(const vector<string> &lines) {
+
+    map<char, pair<int, int>> map_diff = {
+        {'^', {0, -1}},
+        {'>', {1, 0}},
+        {'<', {-1, 0}},
+        {'v', {0, 1}},
+    };
+
+    map<char, char> map_turn = {
+        {'^', '>'},
+        {'>', 'v'},
+        {'<', '^'},
+        {'v', '<'},
+    };
+
+    vector<vector<char>> map_guard;
+    set<pair<int, int>> visited;
+
     for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line);
-        string token;
-
         // put the points in here
-        vector<string> points;
+        vector<char> line_guard;
 
-        // separate line
-        while (getline(ss, token, ',')) { points.push_back(token); }
+        for (const auto &c: line) {line_guard.push_back(c);}
+
+        map_guard.push_back(line_guard);
     }
 
-    cout << "TODO IMPLEMENT" << endl;
+    int guard_x = 36; int guard_y = 52;
+    vector<vector<char>> map_now = map_guard;
+
+    while (true) {
+        // keep track of unique visited locations
+        visited.insert({guard_x, guard_y});
+
+        // move
+        const int x_diff = map_diff[map_now[guard_y][guard_x]].first;
+        const int y_diff = map_diff[map_now[guard_y][guard_x]].second;
+
+        // out of the map
+        if (!(guard_x + x_diff >= 0 && guard_y + y_diff >= 0 && guard_x + x_diff < map_now[0].size() &&
+            guard_y + y_diff < map_now.size())) break;
+
+        // turn
+        if (map_now[guard_y + y_diff][guard_x + x_diff] == '#') {
+            map_now[guard_y][guard_x] = map_turn[map_now[guard_y][guard_x]];
+        } else {
+            map_now[guard_y + y_diff][guard_x + x_diff] = map_now[guard_y][guard_x];
+            map_now[guard_y][guard_x] = '.';
+            guard_x += x_diff;
+            guard_y += y_diff;
+        }
+    }
+
+    cout << "Part 1: " << visited.size() << endl;
+
+    // PART 2
+
+    int part_2 = 0;
+
+    pair<int, int> obstacle;
+    for (int y_obstacle = 0; y_obstacle < map_guard.size(); ++y_obstacle) {
+        cout << y_obstacle << endl; // this is just to track progress
+        for (int x_obstacle = 0; x_obstacle < map_guard[0].size(); ++x_obstacle) {
+            guard_x = 36; guard_y = 52;
+            if (y_obstacle == guard_y && x_obstacle == guard_x) continue;
+
+            // copy map
+            map_now = map_guard;
+
+            // keep path for loops
+            set<pair<char, pair<int, int>>> path_guard;
+
+            while (true) {
+                // if you have a loop break
+                if (path_guard.find(make_pair<char, pair<int,int>>(move(map_now[guard_y][guard_x]),
+                    make_pair<int, int>(move(guard_x), move(guard_y)))) != path_guard.end()) {
+                    part_2++; break;
+                }
+
+                // keep track of path
+                path_guard.insert({map_now[guard_y][guard_x], {guard_x, guard_y}});
+
+                // move
+                const int x_diff = map_diff[map_now[guard_y][guard_x]].first;
+                const int y_diff = map_diff[map_now[guard_y][guard_x]].second;
+
+                // out of the map
+                if (!(guard_x + x_diff >= 0 && guard_y + y_diff >= 0 && guard_x + x_diff < map_now[0].size() &&
+                    guard_y + y_diff < map_now.size())) break;
+
+                // turn
+                if (map_now[guard_y + y_diff][guard_x + x_diff] == '#' || (guard_y + y_diff == y_obstacle && guard_x + x_diff == x_obstacle)) {
+                    map_now[guard_y][guard_x] = map_turn[map_now[guard_y][guard_x]];
+                } else {
+                    map_now[guard_y + y_diff][guard_x + x_diff] = map_now[guard_y][guard_x];
+                    map_now[guard_y][guard_x] = '.';
+                    guard_x += x_diff;
+                    guard_y += y_diff;
+                }
+            }
+        }
+    }
+
+
+
+
+
+    cout << "Part 2: " << part_2 << endl;
 }
