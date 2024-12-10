1f1c68f50ebfdc239040be1ee17e8499068d35b9
Pim van Leeuwen <git@pvleeuwen.com>
2024-12-10 06:20:10 +0100

    Completed day 10, quick one (I basically did part 2 first at part 1, so adapting for part 2 was easy then)

--- day_10/Day10.cpp
+++ day_10/Day10.cpp
@@ -1,25 +1,60 @@
 #include "Day10.h"
 
 #include <algorithm>
+#include <deque>
 #include <iostream>
 #include <sstream>
 #include <map>
 #include <ostream>
+#include <set>
 
 using namespace std;
 
-void Day10::execute(const vector<string> &lines) {
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line);
-        string token;
+pair<int, int> amount_trailheads(vector<vector<int>>& trails, int y, int x) {
+    deque<pair<int,int>> q = {{x,y}};
+    set<pair<int,int>> visited;
 
-        // put the points in here
-        vector<string> points;
+    int trailheads = 0;
 
-        // separate line
-        while (getline(ss, token, ',')) { points.push_back(token); }
+    while (!q.empty()) {
+        auto [x_n, y_n] = q.front();
+        q.pop_front();
+
+        if (trails[y_n][x_n] == 9) { trailheads++; visited.insert({x_n, y_n}); continue; }
+
+        if (x_n - 1 >= 0 && trails[y_n][x_n - 1] == trails[y_n][x_n] + 1) { q.emplace_back(x_n - 1, y_n); }
+        if (x_n + 1 < trails[0].size() && trails[y_n][x_n + 1] == trails[y_n][x_n] + 1) { q.emplace_back(x_n + 1, y_n); }
+        if (y_n - 1 >= 0 && trails[y_n - 1][x_n] == trails[y_n][x_n] + 1) { q.emplace_back(x_n, y_n - 1); }
+        if (y_n + 1 < trails.size() && trails[y_n + 1][x_n] == trails[y_n][x_n] + 1) { q.emplace_back(x_n, y_n + 1); }
     }
 
-    cout << "TODO IMPLEMENT" << endl;
+    return {visited.size(), trailheads};
+
+}
+
+void Day10::execute(const vector<string> &lines) {
+
+    vector<vector<int>> trail_map;
+
+    int part_1 = 0;
+    int part_2 = 0;
+
+    for (const auto &line: lines) {
+        // put the points in here
+        vector<int> points;
+        for (const char c : line) points.push_back(c - '0');
+        trail_map.push_back(points);
+    }
+
+    for (int y = 0; y < trail_map.size(); y++) {
+        for (int x = 0; x < trail_map[y].size(); x++) {
+            if (trail_map[y][x] == 0) {
+                auto [part_1_trails, part_2_trails] = amount_trailheads(trail_map, y, x);
+                part_1 += part_1_trails; part_2 += part_2_trails;
+            }
+        }
+    }
+
+    cout << "Part 1: " << part_1 << endl;
+    cout << "Part 2: " << part_2 << endl;
 }
