30f8d06ff9cc6f5c15d07c9ab0e64f7684c37c7b
Pim van Leeuwen <git@pvleeuwen.com>
2024-12-19 06:37:20 +0100

    Completed day 19

--- day_19/Day19.cpp
+++ day_19/Day19.cpp
@@ -9,18 +9,41 @@
 
 using namespace std;
 
+long can_design(const string& towel, const vector<string>& towels) {
+    vector<long> dp(towel.length() + 1, 0);
+    dp[0] = 1;
+
+    for (int i = 1; i <= towel.length(); ++i) {
+        for (const string& t : towels) {
+            if (i >= t.length() && towel.substr(i - t.length(), t.length()) == t) {
+                dp[i] += dp[i - t.length()];
+            }
+        }
+    }
+    return dp[towel.length()];
+}
+
 void Day19::execute(const vector<string> &lines) {
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line);
-        string token;
 
-        // put the points in here
-        vector<string> points;
+    long part_1 = 0, part_2 = 0;
 
-        // separate line
-        while (getline(ss, token, ',')) { points.push_back(token); }
+    vector<string> towels;
+
+    stringstream ss(lines[0]); string token;
+
+    // separate line
+    while (getline(ss, token, ',')) {
+        token.erase(remove(token.begin(), token.end(), ' '), token.end()); towels.push_back(token);
     }
 
-    cout << "TODO IMPLEMENT" << endl;
+    vector designs(lines.begin()+2, lines.end());
+
+    for (const string& t : designs) {
+        if (can_design(t, towels) > 0) part_1++;
+        part_2 += can_design(t, towels);
+    }
+
+
+    cout << "Part 1: " << part_1 << endl;
+    cout << "Part 2: " << part_2 << endl;
 }
