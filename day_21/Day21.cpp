49f2b514bb8eb3b58c44baa78240546fdcdca85f
Pim van Leeuwen <git@pvleeuwen.com>
2024-12-22 07:24:22 +0100

    Completed day 22 (& 21 which I forgot to push yesterday)

--- day_21/Day21.cpp
+++ day_21/Day21.cpp
@@ -3,21 +3,145 @@
 #include <iostream>
 #include <sstream>
 #include <ostream>
+#include <deque>
+#include <map>
+#include <queue>
 
 using namespace std;
 
-void Day21::execute(const vector<string> &lines) {
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line);
-        string token;
+long hash_function(const int y, const int x, const int n_y, const int n_x, const int nr_of_robots) {
+    long result = y;
+    result *= 4;
+    result += x;
+    result *= 4;
+    result += n_y;
+    result *= 4;
+    result += n_x;
+    result *= 30;
+    result += nr_of_robots;
+    return result;
+}
 
-        // put the points in here
-        vector<string> points;
+map<long, long> memo;
 
-        // separate line
-        while (getline(ss, token, ',')) { points.push_back(token); }
+long cheapest_dir_pad(int y, int x, int des_y, int des_x, int nr_of_robots);
+
+// find the cheapest robot for a string of presses
+long cheapest_robot(const std::string& presses, const int nr_of_robots) {
+    if (nr_of_robots == 1) return static_cast<long>(presses.length());
+
+    long result = 0;
+    const string padConfig = "X^A<v>";
+
+    int y = 0; int x = 2;
+
+    for (int i = 0; i < presses.length(); i++) {
+        for (int n_y = 0; n_y < 2; n_y++) {
+            for (int n_x = 0; n_x < 3; n_x++) {
+                if (padConfig[n_y * 3 + n_x] == presses[i]) {
+                    result += cheapest_dir_pad(y, x, n_y, n_x, nr_of_robots);
+                    y = n_y; x = n_x;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+long cheapest_dir_pad(const int y, const int x, const int des_y, const int des_x, const int nr_of_robots) {
+    const long h = hash_function(y, x, des_y, des_x, nr_of_robots);
+    if (const auto it = memo.find(h); it != memo.end()) return it->second;
+
+    long answer = INT64_MAX;
+
+    deque<tuple<int, int, string>> q; q.emplace_back(x, y, "");
+
+    while (!q.empty()) {
+        auto [n_x, n_y, history] = q.front(); q.pop_front();
+        if (n_y == des_y && n_x == des_x) {
+            long rec = cheapest_robot(history + "A", nr_of_robots-1);
+            answer = min(answer, rec);
+        } else if (!(n_y == 0 && n_x == 0)) {
+            if (n_y < des_y) q.emplace_back( n_x,n_y + 1,history + "v" );
+            else if (n_y > des_y) q.emplace_back( n_x,n_y - 1, history + "^" );
+            if (n_x < des_x) q.emplace_back( n_x + 1,n_y, history + ">" );
+            else if (n_x > des_x) q.emplace_back( n_x - 1,n_y, history + "<" );
+        }
     }
 
-    cout << "TODO IMPLEMENT" << endl;
+    memo[h] = answer; return answer;
+}
+
+long move(const int y, const int x, const int des_y, const int des_x, const int nr_of_robots) {
+    vector<vector<char>> output;
+    deque<tuple<int, int, string>> q; q.emplace_back(x, y, "");
+
+    long answer = INT64_MAX;
+
+    while (!q.empty()) {
+        auto [n_x, n_y, history] = q.front(); q.pop_front();
+
+        if (n_x == des_x && n_y == des_y) {
+            long rec = cheapest_robot(history + "A", nr_of_robots);
+            answer = min(answer, rec);
+        } else if (!(n_x == 0 && n_y == 3)) {
+            if (n_y < des_y) q.emplace_back( n_x,n_y + 1,history + "v");
+            else if (n_y > des_y) q.emplace_back( n_x,n_y - 1, history + "^" );
+            if (n_x < des_x) q.emplace_back( n_x + 1, n_y, history + ">" );
+            else if (n_x > des_x) q.emplace_back( n_x - 1, n_y, history + "<" );
+        }
+    }
+
+    return answer;
+}
+
+void Day21::execute(const vector<string> &lines) {
+
+    long part_1 = 0, part_2 = 0;
+
+    for (const auto &line: lines) {
+        // convert to number
+        string s; for (const char c : line) s += c;
+        stringstream ss(s);
+        int code;
+        ss >> code;
+
+        long result = 0;
+        string padConfig = "789456123X0A";
+
+        int y = 3, x = 2;
+
+        for (const char i : line) {
+            for (int n_y = 0; n_y < 4; n_y++) {
+                for (int n_x = 0; n_x < 3; n_x++) {
+                    if (padConfig[n_y * 3 + n_x] == i) {
+                        result += move(y, x, n_y, n_x, 3);
+                        y = n_y;
+                        x = n_x;
+                    }
+                }
+            }
+        }
+
+        part_1 += result * code;
+
+        // part 2
+        result = 0;
+        y = 3; x = 2;
+
+        for (const char i : line) {
+            for (int n_y = 0; n_y < 4; n_y++) {
+                for (int n_x = 0; n_x < 3; n_x++) {
+                    if (padConfig[n_y * 3 + n_x] == i) {
+                        result += move(y, x, n_y, n_x, 26);
+                        y = n_y; x = n_x;
+                    }
+                }
+            }
+        }
+
+        part_2 += result * code;
+    }
+
+    cout << "Part 1: " << part_1 << endl << "Part 2: " << part_2 << endl;
 }
--- day_22/Day22.cpp
+++ day_22/Day22.cpp
@@ -2,22 +2,76 @@
 
 #include <iostream>
 #include <map>
+#include <set>
 #include <sstream>
 
 using namespace std;
 
+struct Buyer {
+    vector<long> price_history;
+    vector<int> actual_price_history;
+    map<tuple<int, int, int, int>, int> price_map;
+};
+
+void price_change(Buyer &b) {
+    long new_price = b.price_history[b.price_history.size()-1];
+    new_price = ((new_price * 64) ^ new_price) % 16777216;
+    new_price = ((new_price / 32) ^ new_price) % 16777216;
+    new_price = ((new_price * 2048) ^ new_price) % 16777216;
+
+    b.price_history.push_back(new_price);
+
+    const string actual_price = to_string(new_price);
+    const int actual_price_int = actual_price[actual_price.size()-1] - '0';
+    b.actual_price_history.push_back(actual_price_int);
+
+    if (b.actual_price_history.size() > 4) {
+        const tuple<int, int, int, int> change = make_tuple(
+            b.actual_price_history[b.actual_price_history.size()-5] - b.actual_price_history[b.actual_price_history.size()-4],
+            b.actual_price_history[b.actual_price_history.size()-4] - b.actual_price_history[b.actual_price_history.size()-3],
+            b.actual_price_history[b.actual_price_history.size()-3] - b.actual_price_history[b.actual_price_history.size()-2],
+            b.actual_price_history[b.actual_price_history.size()-2] - b.actual_price_history[b.actual_price_history.size()-1]
+            );
+        if (b.price_map.find(change) == b.price_map.end()) b.price_map[change] = actual_price_int;
+    }
+}
+
 void Day22::execute(const vector<string> &lines) {
-    for (const auto &line: lines) {
-        // convert into a string stream to split on token
-        stringstream ss(line);
-        string token;
 
-        // put the points in here
-        vector<string> points;
+    long part_1 = 0, part_2 = 0;
 
-        // separate line
-        while (getline(ss, token, ',')) { points.push_back(token); }
+    // PART 1
+
+    vector<Buyer> buyers; buyers.reserve(lines.size());
+    for (const auto &line: lines) buyers.push_back(Buyer{{stoi(line)}, {line[line.size()-1] - '0'}});
+
+    for (int i = 0; i < 2000; i++) for (Buyer &b : buyers) price_change(b);
+
+    for (const auto &[price_history, actual_price_history, price_map]: buyers) part_1 += price_history[price_history.size()-1];
+
+    cout << "Part 1: " << part_1 << endl;
+
+    // PART 2
+
+    set<tuple<int, int, int, int>> price_map_options;
+
+    // gather all tuple options
+    for (const auto &[price_history, actual_price_history, price_map]: buyers) {
+        for (const auto&[history, price] : price_map) {
+            price_map_options.insert(history);
+        }
     }
 
-    cout << "TODO IMPLEMENT" << endl;
+    // check which one gives most bananas
+    for (const auto& option : price_map_options) {
+        long banana = 0;
+        for (const auto &[price_history, actual_price_history, price_map]: buyers) {
+            if (price_map.find(option) != price_map.end()) {
+                banana += price_map.at(option);
+            }
+        }
+        part_2 = max(part_2, banana);
+    }
+
+    cout << "Part 2: " << part_2 << endl;
 }
